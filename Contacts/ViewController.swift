6f15dc3a922e40a0fa7adfde4ded8bc98d55b984
leilaaua <leila0777@icloud.com>
2021-09-07 19:27:09 +0400

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by leila leila on 07.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

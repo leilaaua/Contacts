e78473c57fc7a6291075bc5dbf2a60a18f49f9da
leilaaua <leila0777@icloud.com>
2021-09-07 21:38:22 +0400

    Create Modals

---
+++ Contacts/Person.swift
@@ -0,0 +1,48 @@
+//
+//  Person.swift
+//  Contacts
+//
+//  Created by leila leila on 07.09.2021.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getPersonData() -> Person {
+        Person(name: personsList.names.randomElement() ?? "",
+               surname: personsList.surnames.randomElement() ?? "",
+               email: personsList.emails.randomElement() ?? "",
+               phoneNumber: personsList.phoneNumbers.randomElement() ?? "")
+    }
+}
+
+class DataManager {
+    let names: [String]
+    let surnames: [String]
+    let emails: [String]
+    let phoneNumbers: [String]
+    
+    init(names: [String], surnames: [String], emails: [String], phoneNumbers: [String]) {
+        self.names = names
+        self.surnames = surnames
+        self.emails = emails
+        self.phoneNumbers = phoneNumbers
+    }
+}
+
+let personsList = DataManager (names: ["John", "Aaron", "Tim", "Ted", "Steven"],
+                               surnames: ["Smith", "Dow", "Isaacson", "Pennyworth", "Jankins"],
+                               emails: ["john@icloud.com, aaron@icloud.com, tim@icloud.com, ted@icloud.com, steven@icloud.com"],
+                               phoneNumbers: ["0555557777", "0453475386", "2748495937", "4648596968", "3564756476"])
+
+
+
